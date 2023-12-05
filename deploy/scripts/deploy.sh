793ffa8e87e264ed01fa651f225e7b7c229e163d
Denis <xczdenis@gmail.com>
2023-12-05 10:30:04 +0300

    feat: initial

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-cd $SERVER_PROJECT_ROOT_DIR
+# Exit in case of any error
+set -e
 
-docker pull $APP_IMG
+. ./scripts/logger.sh
 
-if [ -z "$(docker network ls | grep $GGG_EXTERNAL_NETWORK)" ]; then
-    docker network create $GGG_EXTERNAL_NETWORK
+log_info "Go to project root directory"
+cd "$SERVER_PROJECT_ROOT_DIR"
+
+log_info "Pull from remote git repository"
+git pull
+
+log_info "Pull docker images"
+docker pull "$APP_IMG"
+
+log_info "Create a shared docker network if it doesn't exist"
+if ! docker network ls | grep -q "$GGG_EXTERNAL_NETWORK"; then
+    docker network create "$GGG_EXTERNAL_NETWORK"
 fi
 
+log_info "Down docker containers"
 COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile default down
+
+log_info "Up docker containers"
 COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile default up -d --build
 
+log_info "Remove all unused containers, networks and images"
 docker system prune -f
